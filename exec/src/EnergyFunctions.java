2d95e33d0f576697fd08b7d21ab5632076ebccfd
othman18 <44571722+othman18@users.noreply.github.com>
2019-04-07 01:56:08 +0300

    Add files via upload

--- exec/src/EnergyFunctions.java
+++ exec/src/EnergyFunctions.java
@@ -1,153 +1,182 @@
 import java.awt.image.BufferedImage;
 import java.lang.Math;
+
 public class EnergyFunctions {
-
-	Cell[][] cellMatrix;
-	int cols, rows;
-	boolean caclculatedCellsAlready = false;
-
-	EnergyFunctions(int width, int height) {
-		cellMatrix = new Cell[width + 2][height + 2];
-
-		this.cols = width;
-		this.rows = height;
-
-		for (int x = 0; x < this.cols + 2; x++) {
-			for (int y = 0; y < this.rows + 2; y++) {
-				cellMatrix[x][y] = new Cell();
-			}
-		}
-	}
-
-	public void printer() {
-		for (int i = 0; i < rows + 2; i++) {
-			for (int j = 0; j < cols + 2; j++) {
-				System.out.print(cellMatrix[j][i] + "\t\t");
-			}
-			System.out.println();
-		}
-	}
-
-	void calculateEnergy(int x, int y) {
-		if (!caclculatedCellsAlready) {
-			System.out.println("Can't calculate energy before calculating the RGB");
-			return;
-		}
-
-		Cell current, original = cellMatrix[x][y];
-		double value = 0, counter = 0;
-		for (int i = x - 1; i <= x + 1; i++) {
-			for (int j = y - 1; j <= y + 1; j++) {
-				if (i == x && j == y) // avoid central cell
-					continue;
-				// System.out.println("i="+i+" ,j="+j);
-				current = cellMatrix[i][j];
-				if (current.blue == -1) // avoid empty cells
-					continue;
-				value += (Math.abs(current.red - original.red) + Math.abs(current.green - original.green)
-						+ Math.abs(current.blue - original.blue)) / 3;
-				counter++;
-			}
-		}
-		value /= counter;
-		original.setEnergy(value);
-	}
-
-	void calculateCells(BufferedImage image) {
-		// calculating RBG values for each pixel in the input image
-		for (int x = 1; x <= cols; x++) {
-			for (int y = 1; y <= rows; y++) {
-				int currentPixel = image.getRGB(x - 1, y - 1);
-				cellMatrix[x][y].setRGB(currentPixel);
-			}
-		}
-		caclculatedCellsAlready = true;
-		System.out.println("calculated RGB");
-
-		// calculating the energy of the input image
-		for (int x = 1; x <= cols; x++) {
-			for (int y = 1; y <= rows; y++) {
-				calculateEnergy(x, y);
-			}
-		}
-
-	}
-
-/*---------------------start of Entropy functions-----------------------*/
-	void calculateCellEntropy() {//call this function after calculating the energy
-		/*
-		 * for each cell we will calculate the pValue and then adding the
-		 * entropy
-		 */
-		//the pvalue
-		for (int x = 1; x <= cols; x++) {
-			for (int y = 1; y <= rows; y++) {
-				calculate_PValue(x, y);
-			}
-		}
-		//adding the entropy after we computed the pvalue
-		for (int x = 1; x <= cols; x++) {
-			for (int y = 1; y <= rows; y++) {
-				cellMatrix[x][y].energy+=entropy(x,y);
-			}
-		}
-
-	}
-
-	private double entropy(int col, int row) {
-		double entropy=0.0;
-		double p;
-		int counter = 0; // ??????????????????? check if must be (normalization)
-		for (int x = col - 4; x <= col + 4; x++) {
-			for (int y = row - 4; y <= row + 4; y++) {
-				if ((x >= 1 && x <= cols) && (y >= 1 && y <= rows)) {
-					counter++;// ????????????
-					p=cellMatrix[x][y].p;
-					entropy += cellMatrix[x][y].p*(Math.log(p));
-				}
-			}
-		}
-		entropy=entropy/counter;//?????????????????????
-		return (entropy*-1);
-	}
-
-	private void calculate_PValue(int col, int row) {
-		double p = 0.0;
-		int counter = 0; // ??????????????????? check if must be (normalization)
-		for (int x = col - 4; x <= col + 4; x++) {
-			for (int y = row - 4; y <= row + 4; y++) {
-				if ((x >= 1 && x <= cols) && (y >= 1 && y <= rows)) {
-					counter++;// ????????????
-					p += cellMatrix[x][y].f;
-				}
-			}
-		}
-		p = p / counter;// ????????????????????????
-		p = (cellMatrix[col][row].f) / p;
-		cellMatrix[col][row].p = p;
-	}
+    
+    Cell[][] cellMatrix;
+    int cols, rows;
+    boolean caclculatedCellsAlready = false;
+    
+    EnergyFunctions(int width, int height) {
+        cellMatrix = new Cell[width + 2][height + 2];
+        
+        this.cols = width;
+        this.rows = height;
+        
+        for (int x = 0; x < this.cols + 2; x++) {
+            for (int y = 0; y < this.rows + 2; y++) {
+                cellMatrix[x][y] = new Cell();
+            }
+        }
+    }
+    
+    public void printer() {
+        for (int i = 0; i < rows + 2; i++) {
+            for (int j = 0; j < cols + 2; j++) {
+                System.out.print(cellMatrix[j][i] + "\t\t");
+            }
+            System.out.println();
+        }
+    }
+    
+    void calculateEnergy(int x, int y) {
+        if (!caclculatedCellsAlready) {
+            System.out.println("Can't calculate energy before calculating the RGB");
+            return;
+        }
+        
+        Cell current, original = cellMatrix[x][y];
+        double value = 0, counter = 0;
+        for (int i = x - 1; i <= x + 1; i++) {
+            for (int j = y - 1; j <= y + 1; j++) {
+                if (i == x && j == y) // avoid central cell
+                    continue;
+                // System.out.println("i="+i+" ,j="+j);
+                current = cellMatrix[i][j];
+                if (current.blue == -1) // avoid empty cells
+                    continue;
+                value += (Math.abs(current.red - original.red) + Math.abs(current.green - original.green)
+                          + Math.abs(current.blue - original.blue)) / 3;
+                counter++;
+            }
+        }
+        value /= counter;
+        original.setEnergy(value);
+    }
+    
+    void calculateCells(BufferedImage image) {
+        // calculating RBG values for each pixel in the input image
+        for (int x = 1; x <= cols; x++) {
+            for (int y = 1; y <= rows; y++) {
+                int currentPixel = image.getRGB(x - 1, y - 1);
+                cellMatrix[x][y].setRGB(currentPixel);
+            }
+        }
+        caclculatedCellsAlready = true;
+        System.out.println("calculated RGB");
+        
+        // calculating the energy of the input image
+        for (int x = 1; x <= cols; x++) {
+            for (int y = 1; y <= rows; y++) {
+                calculateEnergy(x, y);
+            }
+        }
+        
+    }
+    
+    /*---------------------start of Entropy functions-----------------------*/
+    void calculateCellEntropy() {// call this function after calculating the energy
+        /*
+         * for each cell we will calculate the pValue and then adding the entropy
+         */
+        // the pvalue
+        for (int x = 1; x <= cols; x++) {
+            for (int y = 1; y <= rows; y++) {
+                calculate_PValue(x, y);
+            }
+        }
+        // adding the entropy after we computed the pvalue
+        for (int x = 1; x <= cols; x++) {
+            for (int y = 1; y <= rows; y++) {
+                cellMatrix[x][y].energy += entropy(x, y);
+            }
+        }
+        
+    }
+    
+    private double entropy(int col, int row) {
+        double entropy = 0.0;
+        double p;
+        int counter = 0; // ??????????????????? check if must be (normalization)
+        for (int x = col - 4; x <= col + 4; x++) {
+            for (int y = row - 4; y <= row + 4; y++) {
+                if ((x >= 1 && x <= cols) && (y >= 1 && y <= rows)) {
+                    counter++;// ????????????
+                    p = cellMatrix[x][y].p;
+                    entropy += cellMatrix[x][y].p * (Math.log(p));
+                }
+            }
+        }
+        entropy = entropy / counter;// ?????????????????????
+        return (entropy * -1);
+    }
+    
+    private void calculate_PValue(int col, int row) {
+        double p = 0.0;
+        int counter = 0; // ??????????????????? check if must be (normalization)
+        for (int x = col - 4; x <= col + 4; x++) {
+            for (int y = row - 4; y <= row + 4; y++) {
+                if ((x >= 1 && x <= cols) && (y >= 1 && y <= rows)) {
+                    counter++;// ????????????
+                    p += cellMatrix[x][y].f;
+                }
+            }
+        }
+        p = p / counter;// ????????????????????????
+        p = (cellMatrix[col][row].f) / p;
+        cellMatrix[col][row].p = p;
+    }
+    
+    /*----------------------remove seam functions -----------------*/
+    public void colorSeam(Coordinates[] coor, BufferedImage img) {
+        for (Coordinates c : coor) {
+            img.setRGB(c.col, c.row, 0xFFFF33);
+        }
+        System.out.println("colored seam");
+    }
+    
+    public BufferedImage removeVerticalSeam(Coordinates[] coor, BufferedImage img) {
+        BufferedImage bufferedImage = new BufferedImage(img.getWidth() - 1, img.getHeight(),
+                                                        BufferedImage.TYPE_INT_RGB);
+        // coor would be from bottom to top
+        int counter = 0, bias = 0;
+        for (int y = img.getHeight() - 1; y >= 0; y--) {
+            for (int x = 0; x < img.getWidth() - 1; x++) { // smaller after removing the seam
+                if (x == coor[counter].col) { // ignore seam here
+                    bias = 1;
+                }
+                bufferedImage.setRGB(x, y, img.getRGB(x + bias, y));
+            }
+            bias = 0;
+            counter++;
+        }
+        System.out.println("removed vertical seam");
+        return bufferedImage;
+    }
+    /*---------------------- end of remove seam functions -----------------*/
 }
+
 /*---------------------End of Entropy functions-----------------------*/
 class Cell {
-	int red = -1, green = -1, blue = -1;
-	double energy = -1;
-	double M = -1;// this will be used for the dynamic programming computation
-	double f = -1;// this is the greyscale for a pixel
-	double p = -1; // this is for the entropy
-
-	void setRGB(int pixel) {
-		red = (pixel >> 16) & 0xff;
-		green = (pixel >> 8) & 0xff;
-		blue = pixel & 0xff;
-		f = ((double) (red + green + blue)) / 3;
-	}
-
-	void setEnergy(double energy) {
-		this.energy = energy;
-	}
-
-	public String toString() {
-		return "(" + M + "," + energy + ")";
-
-	}
+    int red = -1, green = -1, blue = -1;
+    double energy = -1;
+    double M = -1;// this will be used for the dynamic programming computation
+    double f = -1;// this is the greyscale for a pixel
+    double p = -1; // this is for the entropy
+    
+    void setRGB(int pixel) {
+        red = (pixel >> 16) & 0xff;
+        green = (pixel >> 8) & 0xff;
+        blue = pixel & 0xff;
+        f = ((double) (red + green + blue)) / 3;
+    }
+    
+    void setEnergy(double energy) {
+        this.energy = energy;
+    }
+    
+    public String toString() {
+        return "(" + M + "," + energy + ")";
+        
+    }
 }
--- exec/src/exec.java
+++ exec/src/exec.java
@@ -4,63 +4,75 @@ import java.io.IOException;
 import java.util.Arrays;
 
 import javax.imageio.ImageIO;
+
 /*
- * flip el x ma3 el y
+ * othman's notes 6.4.19
+ * update seam does not work when we trim an image, or add anything to it.
+ * we should make the energy function obj dynamic or we would have to create a new obj each time
+ * we remove a seam. (see example below)
  * 
  * */
 public class exec {
 	public static void main(String[] args) {
-		EnergyFunctions aa=new EnergyFunctions(3,3);
-		aa.cellMatrix[1][1].setRGB(0xf);
-		aa.cellMatrix[1][2].setRGB(0x0);
-		aa.cellMatrix[1][3].setRGB(0xa);
-		aa.cellMatrix[2][1].setRGB(0x2);
-		aa.cellMatrix[2][2].setRGB(0x5);
-		aa.cellMatrix[2][3].setRGB(0xc);
-		aa.cellMatrix[3][1].setRGB(0xf);
-		aa.cellMatrix[3][2].setRGB(0x0);
-		aa.cellMatrix[3][3].setRGB(0x6);
-		aa.caclculatedCellsAlready=true;
-		for(int i=1;i<4;i++){
-			for (int j = 1; j < 4; j++) {
-				aa.calculateEnergy(i, j);
-			}
-		}
-//		System.out.println(Arrays.deepToString(aa.cellMatrix));
-		
-		seamCalculate s=new seamCalculate(aa, seamShape.general);
-		s.updateSeam();
-		aa.printer();
-		System.out.println(Arrays.deepToString(s.coors));
-		s=new seamCalculate(aa, seamShape.straight);
-		s.updateSeam();
-		System.out.println(Arrays.deepToString(s.coors));
-		
-    /*	// read image
+		EnergyFunctions aa = new EnergyFunctions(3, 3);
+		/*
+		 * aa.cellMatrix[1][1].setRGB(0xf); aa.cellMatrix[1][2].setRGB(0x0);
+		 * aa.cellMatrix[1][3].setRGB(0xa); aa.cellMatrix[2][1].setRGB(0x2);
+		 * aa.cellMatrix[2][2].setRGB(0x5); aa.cellMatrix[2][3].setRGB(0xc);
+		 * aa.cellMatrix[3][1].setRGB(0xf); aa.cellMatrix[3][2].setRGB(0x0);
+		 * aa.cellMatrix[3][3].setRGB(0x6); aa.caclculatedCellsAlready=true; for(int
+		 * i=1;i<4;i++){ for (int j = 1; j < 4; j++) { aa.calculateEnergy(i, j); } } //
+		 * System.out.println(Arrays.deepToString(aa.cellMatrix));
+		 * 
+		 * seamCalculate s=new seamCalculate(aa, seamShape.general); s.updateSeam();
+		 * aa.printer(); System.out.println(Arrays.deepToString(s.coors)); s=new
+		 * seamCalculate(aa, seamShape.straight); s.updateSeam();
+		 * System.out.println(Arrays.deepToString(s.coors));
+		 */
+
+		// read image
 		BufferedImage image = null;
 		File inputFile = null;
 		try {
 
-			inputFile = new File("/Users/othman/Downloads/old_photos/img.jpeg");
+			inputFile = new File("/Users/othman/Downloads/image_test/dude.jpg");
 			image = ImageIO.read(inputFile);
-
 			System.out.println("Reading complete.");
 
 		} catch (IOException e) {
 
 			e.printStackTrace();
 		}
-		int  height= image.getWidth();
-		int  width= image.getHeight();
-
-		aa = new EnergyFunctions(height,width);
+		int width = image.getWidth();
+		int height = image.getHeight();
+		System.out.println(width + " , " + height);
+		aa = new EnergyFunctions(width, height);
 		aa.calculateCells(image);
+
+		seamCalculate s = new seamCalculate(aa, seamShape.general);
+		s.updateSeam();
+		aa.colorSeam(s.coors, image);
 		// write image
 		try {
-			inputFile = new File("/Users/othman/Downloads/old_photos/out.jpeg");
+			inputFile = new File("/Users/othman/Downloads/image_test/out.jpg");
+			ImageIO.write(image, "jpg", inputFile);
+			inputFile = new File("/Users/othman/Downloads/image_test/removedSeam.jpg");
+			for(int i = 0; i< 50;i++) {
+				aa.colorSeam(s.coors, image);
+				inputFile = new File("/Users/othman/Downloads/image_test/colorout"+i+".jpg");
+				ImageIO.write(image, "jpg", inputFile);
+				image = aa.removeVerticalSeam(s.coors, image);
+				inputFile = new File("/Users/othman/Downloads/image_test/out"+i+".jpg");
+				ImageIO.write(image, "jpg", inputFile);
+				aa = new EnergyFunctions(image.getWidth(), image.getHeight());
+				aa.calculateCells(image);
+				s = new seamCalculate(aa, seamShape.general);
+				s.updateSeam();
+			}
 			ImageIO.write(image, "jpg", inputFile);
 		} catch (IOException e) {
 			System.out.println(e);
-		}*/
+		}
+		System.out.println("writing complete");
 	}
 }
