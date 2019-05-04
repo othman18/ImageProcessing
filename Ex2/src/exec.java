76f5754962e47cf4d5f8686918e7d2295d71f856
othman <othman_watad@hotmail.com>
2019-05-04 21:22:29 +0300

    defined a the plane's normal and offset

--- Ex2/src/exec.java
+++ Ex2/src/exec.java
@@ -3,14 +3,13 @@ import surfaces.*;
 
 public class exec {
 	public static void main(String[] args) {
-		//
 		Vector v1 = new Vector(1, 2, 3);
 		Point p1 = new Point(1, 2, 3);
 		Surfaces s1 = new Sphere(p1, 0);
 		Surfaces s2 = new Triangle(v1, v1, v1);
-		Surfaces s3 = new InfinitePlane();
-		System.out.println(s1.getType());
-		System.out.println(s2.getType());
-		System.out.println(s3.getType());
+		Surfaces s3 = new InfinitePlane(10,20,30);
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s3);
 	}
 }
--- Ex2/src/surfaces/InfinitePlane.java
+++ Ex2/src/surfaces/InfinitePlane.java
@@ -1,20 +1,29 @@
 package surfaces;
 
-public class InfinitePlane extends Surfaces{
-	
-	public InfinitePlane() {
+public class InfinitePlane extends Surfaces {
+
+	double a, b, c; // ax+by + c = 0
+
+	/**
+	 * input is a vector where the first two elements are the normal and the third
+	 * element is the factor
+	 */
+	public InfinitePlane(double a, double b, double c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+
 		System.out.println("should define the normal and offset");
 	}
 
 	public type getType() {
 		return type.infinitePlane;
 	}
-	
+
 	@Override
 	public String toString() {
-		return "IP.: N=?, P=?"; 
-		
-	}
+		return "IP.: " + a + "*x + " + b + "*y " + c + " = 0";
 
+	}
 
 }
