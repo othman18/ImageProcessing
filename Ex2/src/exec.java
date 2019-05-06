2c66e6b4cbe0661002e3b33e14b8869f532febe6
othman <othman_watad@hotmail.com>
2019-05-06 15:24:04 +0300

    update

--- Ex2/src/exec.java
+++ Ex2/src/exec.java
@@ -7,7 +7,7 @@ public class exec {
 		Point p1 = new Point(1, 2, 3);
 		Surfaces s1 = new Sphere(p1, 0);
 		Surfaces s2 = new Triangle(v1, v1, v1);
-		Surfaces s3 = new InfinitePlane(10,20,30);
+		Surfaces s3 = new InfinitePlane(10,20,30,0);
 		System.out.println(s1);
 		System.out.println(s2);
 		System.out.println(s3);
